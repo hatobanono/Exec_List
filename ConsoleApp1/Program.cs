f722cd80c6ae910a32515423fe1daa8a06465c87
hatobanono <chris00iq@gmail.com>
2022-10-24 18:45:41 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Product> products = new List<Product>();
+		}
+	}
+
+	class Product
+	{
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+		public int Amount { get; set; }
+	}
+}
